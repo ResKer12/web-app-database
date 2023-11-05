852b8cf1b5d0b1cb54d8f705c7800db98af2bd4f
ResKer12 <maximillian.dewar@gmail.com>
2023-11-05 15:14:45 +0000

    Updated Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(UserTableSeeder::class);
         $this->call(PostTableSeeder::class);
         $this->call(CommentTableSeeder::class);
         $this->call(TagTableSeeder::class);
